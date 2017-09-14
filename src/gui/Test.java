8566d48166976f66e3e15268b1367bb93329ce11
chemry <16097448d@connect.polyu.hk>
2017-09-14 17:34:05 +0800

    setup all the playerdots have some idea about the whole frame~~

--- src/gui/Test.java
+++ src/gui/Test.java
@@ -7,6 +7,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 
@@ -28,7 +30,7 @@ public class Test {
     private JLabel player112;
     private JLabel player115;
     private JLabel player114;
-    private JLabel player6;
+    private JLabel player116;
     private JLabel player126;
     private JLabel player125;
     private JLabel player124;
@@ -41,22 +43,143 @@ public class Test {
     private JLabel player133;
     private JLabel player132;
     private JLabel player131;
+    private JLabel player16;
+    private JLabel player15;
+    private JLabel player14;
+    private JLabel player13;
+    private JLabel player12;
+    private JLabel player11;
+    private JLabel player26;
+    private JLabel player25;
+    private JLabel player24;
+    private JLabel player23;
+    private JLabel player22;
+    private JLabel player21;
+    private JLabel player36;
+    private JLabel player35;
+    private JLabel player34;
+    private JLabel player33;
+    private JLabel player32;
+    private JLabel player31;
+    private JLabel player46;
+    private JLabel player45;
+    private JLabel player44;
+    private JLabel player43;
+    private JLabel player42;
+    private JLabel player41;
+    private JLabel player56;
+    private JLabel player55;
+    private JLabel player54;
+    private JLabel player53;
+    private JLabel player52;
+    private JLabel player51;
+    private JLabel player64;
+    private JLabel player66;
+    private JLabel player65;
+    private JLabel player63;
+    private JLabel player62;
+    private JLabel player61;
+    private JLabel player71;
+    private JLabel player76;
+    private JLabel player75;
+    private JLabel player74;
+    private JLabel player73;
+    private JLabel player72;
+    private JLabel player86;
+    private JLabel player85;
+    private JLabel player84;
+    private JLabel player83;
+    private JLabel player82;
+    private JLabel player81;
+    private JLabel player91;
+    private JLabel player92;
+    private JLabel player93;
+    private JLabel player96;
+    private JLabel player95;
+    private JLabel player94;
+    private JLabel player106;
+    private JLabel player105;
+    private JLabel player104;
+    private JLabel player103;
+    private JLabel player102;
+    private JLabel player101;
+    private JLabel player146;
+    private JLabel player145;
+    private JLabel player144;
+    private JLabel player143;
+    private JLabel player142;
+    private JLabel player141;
+    private JLabel player156;
+    private JLabel player155;
+    private JLabel player154;
+    private JLabel player153;
+    private JLabel player152;
+    private JLabel player151;
+    private JLabel player166;
+    private JLabel player165;
+    private JLabel player164;
+    private JLabel player163;
+    private JLabel player162;
+    private JLabel player161;
+    private JLabel player176;
+    private JLabel player175;
+    private JLabel player174;
+    private JLabel player173;
+    private JLabel player172;
+    private JLabel player171;
+    private JLabel player186;
+    private JLabel player185;
+    private JLabel player184;
+    private JLabel player183;
+    private JLabel player182;
+    private JLabel player181;
+    private JLabel player196;
+    private JLabel player195;
+    private JLabel player194;
+    private JLabel player193;
+    private JLabel player192;
+    private JLabel player191;
+    private JLabel player206;
+    private JLabel player205;
+    private JLabel player204;
+    private JLabel player203;
+    private JLabel player202;
+    private JLabel player201;
+    private JButton button01;
+    private JButton button02;
+    private JButton button03;
+    private JButton button4;
+    private JButton testButton;
     private static int cnt = 0;
     Dice dice = new Dice(dice0, dice1, counter, TextField1);
-
+    Map<String, JLabel> labelMap = new HashMap<String, JLabel>();
 
 
     private boolean field1Vis = false;
     public Test() {
-        trying();
+        //trying();
         try {
-            Field temp = this.getClass().getDeclaredField("player111");
-            temp.setAccessible(true);
-            JLabel tempLabel;
-            tempLabel = (JLabel) temp.get(this);
-            tempLabel.setVisible(false);
-        } catch (Throwable e) {
-            System.out.println(e);
+            for(int i = 1; i <= 20; i++) {
+                for(int j = 1; j <= 6; j++) {
+                    String name = "player" + i + j;
+                    Field temp = this.getClass().getDeclaredField(name);
+                    temp.setAccessible(true);
+                    JLabel tempLabel;
+                    tempLabel = (JLabel) temp.get(this);
+                    //tempLabel.setVisible(false);
+                    labelMap.put(name, tempLabel);
+                    /*try {
+                        TimeUnit.MILLISECONDS.sleep(10);
+                        cnt++;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }*/
+
+                }
+            }
+
+        } catch (Throwable e2) {
+            System.out.println("here" + e2);
         }
 
         counter.setText("Auto stop in:\n" + 5);
@@ -88,7 +211,7 @@ public class Test {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if(startButton.getText() == "Start!"){
-                    System.out.println(panel1.getSize());
+                    System.out.println(field1.getSize());
                     System.out.print("good");
                     if(field1Vis == false) {
 
@@ -102,9 +225,18 @@ public class Test {
 
             }
         });
+
+        /*testButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //testing();
+                labelMap.get("player123").setVisible(false);
+
+            }
+        });*/
     }
 
-    public void trying(){
+    public void testing(){
         /*try {
             Field temp = this.getClass().getDeclaredField("player112");
 
@@ -113,8 +245,29 @@ public class Test {
         } catch (Throwable e) {
             System.out.println(e);
         }*/
-        for(Field fields : this.getClass().getDeclaredFields())
-            System.out.println(fields);
+        try {
+            for(int i = 1; i <= 20; i++) {
+                for(int j = 1; j <= 6; j++) {
+                    Field temp = this.getClass().getDeclaredField("player" + i + j);
+                    temp.setAccessible(true);
+                    JLabel tempLabel;
+                    tempLabel = (JLabel) temp.get(this);
+                    tempLabel.setVisible(false);
+                    /*try {
+                        TimeUnit.MILLISECONDS.sleep(10);
+                        cnt++;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }*/
+
+                }
+            }
+
+        } catch (Throwable e2) {
+            System.out.println(e2);
+        }
+
+
     }
 
 
@@ -131,10 +284,11 @@ public class Test {
         Container cp = frame.getContentPane();
         //cp.setLayout(new BorderLayout());
         System.out.println(img.getIconHeight() + " " +img.getIconWidth());
-
-        JButton but = new JButton();
         ((JPanel)cp).setOpaque(false);
 
     }
 
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
